952126cd8ad3fce5c59bfebcba074f6b4545e11b
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-21 15:05:34 +0100

    UPDATED: link to component/footer

--- app/[lng]/page.jsx
+++ app/[lng]/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useTranslation } from "../i18n";
-import { Footer } from "./components/Footer";
+import { Footer } from "./components/footer";
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng);
